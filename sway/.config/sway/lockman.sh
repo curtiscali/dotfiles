7e7bd5d13887b196c6c44eeb3bb3cc6ba29627a9
Curtis Cali <curtiscali@pm.me>
2023-03-16 19:11:16 -0700

    Updated format for dates on lock screen

--- sway/.config/sway/lockman.sh
+++ sway/.config/sway/lockman.sh
@@ -3,6 +3,6 @@
        timeout 10 'swaymsg "output * dpms off"' \
            resume 'swaymsg "output * dpms on"' &
  # Locks the screen immediately
-swaylock --clock --indicator --screenshots --effect-scale 0.4 --effect-vignette 0.2:0.5 --effect-blur 4x2 --datestr "%a %e.%m.%Y" --timestr "%k:%M"
+swaylock --clock --indicator --screenshots --effect-scale 0.4 --effect-vignette 0.2:0.5 --effect-blur 4x2 --datestr "%a %B %d %Y" --timestr "%H:%M"
 # Kills last background task so idle timer doesn't keep running
 kill %%
