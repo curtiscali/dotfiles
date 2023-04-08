c527e2f9688eeacccf9d040ae4a128ad3fb9fb08
Curtis Cali <curtiscali@pm.me>
2023-04-07 19:02:58 -0700

    switched to using a python script

---
+++ sway/.config/sway/scripts/query-fcitx5-ime.py
@@ -0,0 +1,19 @@
+import subprocess
+import time
+import json
+import sys
+
+ime_labels = {
+    'keyboard-us': 'EN',
+    'pinyin': '中'
+}
+
+while True:
+    current_ime_cmd = subprocess.run(['fcitx5-remote', '-n'], capture_output=True)
+    current_ime = current_ime_cmd.stdout.decode('utf-8').strip()
+
+    output_data = { 'text': f'{ime_labels[current_ime]}' }
+    print(json.dumps(output_data))
+    sys.stdout.flush()
+
+    time.sleep(0.2)
