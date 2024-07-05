609a13d6f46e7c88540e4bd9a78399d1a3529503
Curtis Cali <curtiscali@pm.me>
2024-07-05 16:04:33 -0700

    Updated waybar ime module config

--- waybar/.config/waybar/scripts/query-fcitx5-ime.py
+++ waybar/.config/waybar/scripts/query-fcitx5-ime.py
@@ -13,7 +13,6 @@ while True:
     current_ime = current_ime_cmd.stdout.decode('utf-8').strip()
 
     output_data = { 'text': f'{ime_labels[current_ime]}' }
-    print(json.dumps(output_data))
-    sys.stdout.flush()
+    print(json.dumps(output_data), flush=True)
 
     time.sleep(0.2)
