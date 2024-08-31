e1f71f8f874e323470770098b6f6e333117d06e5
Curtis Cali <curtiscali@pm.me>
2024-08-31 16:08:49 -0700

    Added COC.NVIM to NVIM

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -18,8 +18,8 @@ require("lazy").setup({
     -- import your plugins
     {
     	'Mofiqul/vscode.nvim',
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
+	    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    	priority = 1000, -- make sure to load this before all the other start plugins
     	config = function()
     	    -- load the colorscheme here
 	        vim.cmd([[colorscheme vscode]])
@@ -97,6 +97,10 @@ require("lazy").setup({
     {
     	'nvim-telescope/telescope.nvim', tag = '0.1.8',
       	dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+    {
+        'neoclide/coc.nvim',
+        branch = 'release'
     }
   },
   -- Configure any other settings here. See the documentation for more details.
