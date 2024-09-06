26395cbb2040fa4c2da74a79a451c7b1c105becf
Curtis Cali <curtiscali@pm.me>
2024-09-05 20:30:43 -0700

    Updated lock settings for sway to suspend & Lock PC when needed

--- sway/.config/sway/scripts/lockman.sh
+++ sway/.config/sway/scripts/lockman.sh
@@ -3,6 +3,7 @@
 #Locks the screen off and puts it to background
  swayidle \
        timeout 10 'swaymsg "output * dpms off"' \
+       timeout 3600 'systemctl suspend' \
            resume 'swaymsg "output * dpms on"' &
  # Locks the screen immediately
 swaylock -c 1B1C1F
