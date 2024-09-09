04ab6c1a29d7cb5a9c3b6970ea52d9be248b0610
Curtis Cali <curtiscali@pm.me>
2024-09-08 20:02:40 -0700

    remove suspend calls

--- sway/.config/sway/scripts/lockman.sh
+++ sway/.config/sway/scripts/lockman.sh
@@ -3,8 +3,7 @@
 #Locks the screen off and puts it to background
  swayidle \
        timeout 10 'swaymsg "output * power off"' \
-       timeout 3600 'systemctl suspend' \
-           resume 'swaymsg "output * power on"' &
+       resume 'swaymsg "output * power on"' &
 # Locks the screen immediately
 swaylock -f -k -l --font "Cantarell Bold" --font-size 16 -i /home/cucali/Pictures/Lock_Screen_Backgrounds/gdm_background.png
 
