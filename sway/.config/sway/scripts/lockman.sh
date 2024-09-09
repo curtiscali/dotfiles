f013117659a482696994781cb736eedb89e615eb
Curtis Cali <curtiscali@pm.me>
2024-09-08 21:38:11 -0700

    updated suspend settings

--- sway/.config/sway/scripts/lockman.sh
+++ sway/.config/sway/scripts/lockman.sh
@@ -3,9 +3,11 @@
 #Locks the screen off and puts it to background
  swayidle \
        timeout 10 'swaymsg "output * power off"' \
-       resume 'swaymsg "output * power on"' &
+       timeout 3600 'systemctl suspend' \
+       resume 'swaymsg "output * power on"' \
+       after-resume 'swaymsg reload'&
 # Locks the screen immediately
-swaylock -f -k -l --font "Cantarell Bold" --font-size 16 -i /home/cucali/Pictures/Lock_Screen_Backgrounds/gdm_background.png
+swaylock -k -l --font "Cantarell Bold" --font-size 16 -i /home/cucali/Pictures/Lock_Screen_Backgrounds/gdm_background.png
 
 # Kills last background task so idle timer doesn't keep running
 kill %%;
