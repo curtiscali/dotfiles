8d1132035ac5b1399c9eb5d8072353fa5ebeb4db
Curtis Cali <curtiscali@pm.me>
2024-12-04 21:18:18 -0800

    improved tmux configuration and refined alacritty config

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -17,10 +17,11 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     {
-    	'Mofiqul/vscode.nvim',
+        'Mofiqul/vscode.nvim',
 	    lazy = false, -- make sure we load this during startup if it is your main colorscheme
     	priority = 1000, -- make sure to load this before all the other start plugins
     	config = function()
+
     	    -- load the colorscheme here
 	        vim.cmd([[colorscheme vscode]])
     	end,
