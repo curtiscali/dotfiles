c21375ce384250599ec772273a977a4d87acd936
Curtis Cali <curtiscali@pm.me>
2024-12-05 17:53:46 -0800

    Added prefix highlight to TMUX & changed nvim color scheme

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -17,13 +17,15 @@ require("lazy").setup({
   spec = {
     -- import your plugins
     {
-        'Mofiqul/vscode.nvim',
+        'navarasu/onedark.nvim',
 	    lazy = false, -- make sure we load this during startup if it is your main colorscheme
     	priority = 1000, -- make sure to load this before all the other start plugins
     	config = function()
-
+            require('onedark').setup {
+                style = 'darker'
+            }
     	    -- load the colorscheme here
-	        vim.cmd([[colorscheme vscode]])
+	        vim.cmd([[colorscheme onedark]])
     	end,
     },
     {
@@ -36,7 +38,7 @@ require("lazy").setup({
     {
         'nvim-lualine/lualine.nvim',
     	dependencies = { 'nvim-tree/nvim-web-devicons' },
-        opts = { theme = 'vscode' },
+        opts = { theme = 'onedark' },
     },
     {
         'nvim-treesitter/nvim-treesitter',
@@ -107,7 +109,7 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "vscode" } },
+  install = { colorscheme = { "onedark" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
