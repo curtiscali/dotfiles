80af01230c496b95d07e78b1c665a8c85412f718
Curtis Cali <curtiscali@pm.me>
2024-12-09 20:31:35 -0800

    Improved TMUX formatting & Neovim tabs

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -102,7 +102,23 @@ require("lazy").setup({
         config = function()
             require('colorizer').setup()
         end
-    }
+    },
+    {
+        'romgrk/barbar.nvim',
+        dependencies = {
+          'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+          'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+        },
+        init = function() 
+            vim.g.barbar_auto_setup = false
+        end,
+        opts = {
+          -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+          -- animation = true,
+          -- insert_at_start = true,
+          -- …etc.
+        }
+  }
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
